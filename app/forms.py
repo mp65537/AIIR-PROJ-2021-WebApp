ce71d0fa8d58dae509ff7f0d47bc8acebdf274a3
Szymon Draszkiewicz <SzymonDraszkiewicz9@gmail.com>
2021-06-08 11:11:14 +0200

    alternatywne podejscie: endpointy na backend

--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, FileField
-
-class CompileForm(FlaskForm):
-    link = StringField('link')
-    zip = FileField('zip')
--- app/main.py
+++ app/main.py
@@ -1,56 +1,15 @@
-import os, git, tempfile, time, requests, msgpack
-
-from flask import flash, Flask, send_file, request
-from forms import CompileForm
-from werkzeug.utils import redirect, secure_filename
-
-app = Flask(__name__, static_url_path='')
+import os
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route("/")
 def main():
-    return app.send_static_file('index.html')
-
-@app.route('/compile', methods=['POST'])
-def downlaod():
-    form = CompileForm()
-
-    codefile = tempfile.NamedTemporaryFile(suffix='.zip').name
-    codefilepath = os.path.join(codefile)
-    
-    # link z githuba
-    if 'file' not in request.files:
-        codedir = tempfile.mkdtemp()
-        codedirpath = os.path.join(codedir)
-
-        repo = git.Repo.clone_from(form.link.data, to_path=codedirpath)
-
-        with open(codefilepath, 'wb') as zipfile: 
-            repo.archive(zipfile, format='zip')
-    
-    else:
-        file = request.files['file']
-
-        # ani plik ani link
-        if file.filename == '':
-            return 'No file/link has been provided.'
-
-        # plik
-        else:
-            file.save(codefilepath)
-
-    # compile
-    with open(codefilepath, 'rb') as to_compile:
-        data = msgpack.packb({'zip': to_compile.read()})
-        response = requests.post(
-            'http://py:8080/api', 
-            data=data,
-            headers={'Content-Type': 'application/octet-stream'}
-        )
-        response_data = msgpack.unpackb(response.content)
-
-        # placeholder - sobie czy cokolwiek wgle doszlo
-        return str(response_data)
+    # get base from env vars instead
+    base = os.environ['API']
+    api = base + '/api'
+    artifact = base + '/artifact'
+    return render_template('index.html', api=api, artifact=artifact)
 
 if __name__ == "__main__":
     logging.basicConfig(
